4603ce4c77d5f0614356fbb92cb8f8fe2c5a33d3
iammark <mrdamarkomm@gmail.com>
2023-09-08 16:01:02 +0200

    Starting project with header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import '../components/Header.css'
+export const Header = () => {
+  return (
+    <div className="titles">
+      <h1 id="name">Mrda Marko</h1>
+
+      <h2 id="occupation">
+        FullStack-Developer
+      </h2>
+    </div>
+  );
+};
